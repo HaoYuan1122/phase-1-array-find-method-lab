446e2de407de74845c59d6bd4824cbc2f733159a
Hao Yuan <haomichaelyuan@gmail.com>
2022-09-26 13:26:35 +1000

    commit tree 1f7ad6eb4adbd4a5c5209c995508069a84dd720e

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // code your solution here
+const record = [
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2015", result: "W"},
+
+  ]
+
+
+//   function loopThroughAndFind(inventory,finder){
+//     let result=null
+//     for(let item of inventory){
+//         if(finder(item)===true){
+//             result=item
+//             break
+//         }
+//     }
+//     return result.year
+//   }
+
+//   console.log(loopThroughAndFind(record,superbowlWin))
+
+function superbowlWin(array){
+    function callback(element){
+        return element.result==="W"
+    }
+    const element=array.find(callback)
+    if (element!==undefined){
+        return element.year
+    }
+
+}
+
+console.log(superbowlWin(record))
+//   function callback(element){
+//     return element.result==="W"
+// }
+//  record.find(superbowlWin)
+
+//   console.log(record.find(superbowlWin))
